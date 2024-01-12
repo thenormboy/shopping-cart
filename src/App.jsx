aa69d10c5c1361e1d298d117f3750a4a2b560409
Bleaze Perris <bperriscode@gmail.com>
2024-01-12 18:11:46 -0500

    setup files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header';
+
+import React, { useState } from "react";
+
+const App = () => {
+
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
