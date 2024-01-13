91a4b8a14e80da2532c1f4147b492222a1b603c5
Bleaze Perris <bperriscode@gmail.com>
2024-01-12 19:29:27 -0500

    setup files

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+        <>
+        <div className="header-top">
+            <div className="header-logo">
+                Saturn Tiger
+            </div>
+            <div className="header-cart">
+                M
+            </div>
+        </div>
+        <div className="header-bottom">
+            <>Home</>
+            <>Products</>
+            <>About</>
+        </div>
+        </>
+    )
+}
